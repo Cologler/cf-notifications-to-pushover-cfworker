ad267ed99bbc056507af2e55c6ae8c92a6b0e65e
Cologler <skyoflw@gmail.com>
2024-01-29 02:50:32 +0800

    feat: worked commit

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,72 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	// vars:
+	PUSHOVER_API_URL: string,
+	IS_WEBHOOK_SECRET_USER_KEY?: string,
+	PUSHOVER_MESSAGE_PRIORITY?: string,
+	PUSHOVER_MESSAGE_TITLE?: string,
+
+	// secrets:
+	WEBHOOK_SECRET: string,
+	PUSHOVER_USER_KEY: string,
+	PUSHOVER_APP_TOKEN: string,
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const cfWebhookAuth = request.headers.get("cf-webhook-auth");
+
+		function isWebhookSecretUserKey() {
+			return env.IS_WEBHOOK_SECRET_USER_KEY && env.IS_WEBHOOK_SECRET_USER_KEY != '0';
+		}
+
+		if (isWebhookSecretUserKey()) {
+			if (!cfWebhookAuth) {
+				return new Response("Missing cf-webhook-auth", {
+					headers: {'content-type': 'text/plain'},
+					status: 401
+				})
+			}
+		}
+		else if (env.WEBHOOK_SECRET && cfWebhookAuth !== env.WEBHOOK_SECRET) {
+			return new Response(":(", {
+				headers: {'content-type': 'text/plain'},
+				status: 401
+			})
+		}
+
+		const webhookPayload = await request.json<{
+			text: string
+		}>();
+		const message = webhookPayload.text ?? '<EMPTY>';
+
+		let body: any = {
+			token: env.PUSHOVER_APP_TOKEN,
+			user: isWebhookSecretUserKey() ? cfWebhookAuth : env.PUSHOVER_USER_KEY,
+			message: message,
+		};
+		if (env.PUSHOVER_MESSAGE_PRIORITY) {
+			body.priority = Number(env.PUSHOVER_MESSAGE_PRIORITY);
+		}
+		if (env.PUSHOVER_MESSAGE_TITLE) {
+			body.title = env.PUSHOVER_MESSAGE_TITLE;
+		}
+
+		// pushover API doc: https://pushover.net/api
+		try {
+			const response = await fetch(env.PUSHOVER_API_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			});
+			console.log(`Pushover response: ${response.status} ${response.statusText}`);
+		} catch (error) {
+			console.log(error)
+		}
+
+		return new Response(":)", {
+			headers: {'content-type': 'text/plain'},
+		})
 	},
 };
